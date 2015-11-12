0576b0c907b47c5b11e102061d6ac643e31bacd1
Andrea <andrea.amata@progettoarchimede.it>
2015-11-12 10:09:35 +0100

    Ho aggiunto il terzo bottone

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -48,6 +48,7 @@ namespace Combobox
             this.cittàCB = new System.Windows.Forms.ComboBox();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.NumRigaUD)).BeginInit();
             this.SuspendLayout();
             // 
@@ -230,11 +231,21 @@ namespace Combobox
             this.button2.Text = "button2";
             this.button2.UseVisualStyleBackColor = true;
             // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(474, 33);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 18;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(592, 269);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.cittàCB);
@@ -289,6 +300,7 @@ namespace Combobox
         private System.Windows.Forms.ComboBox cittàCB;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
 
         public EventHandler richTextBox1_TextChanged { get; private set; }
     }
